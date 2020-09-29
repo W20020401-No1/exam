401f8d6556834fb7ed3cb13be6acd368aa20dae4
liucehng-12-08 <1256346867@qq.com>
2020-09-29 11:59:43 +0800

    多选模块

--- src/views/Multil.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        这是多选
-    </div>
-</template>
---
+++ src/views/Multilsingle.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <!-- 答题区域 -->
+    <Multil @on-finish="finish" v-if="!isDone" />
+
+    <!-- 成绩 -->
+    <Multilscore :answers="questions" :qlist="qlist" v-else />
+  </div>
+</template>
+
+<script>
+import Multil from "../components/Multil";
+import Multilscore from "../components/Multilscore";
+import { mapActions } from "vuex";
+export default {
+  name: "App",
+  components: {
+    Multil,
+    Multilscore,
+  },
+  data() {
+    return {
+      isDone: false,
+      questions: new Map(),
+      qlist: [],
+      q:[],
+      score:0
+    };
+  },
+  methods: {
+    ...mapActions(["adds"]),
+    finish: function (q, l) {
+      this.isDone = true
+      this.questions = q
+      this.qlist = l
+      // 其他
+      //
+      for (const q of this.qlist) {
+        if (q.right.join("") === this.questions.get(q.id).join("")) {
+          this.score += 20;
+        }
+      }
+      this.questions.forEach((a) => {
+        this.q.push(a);
+      });
+      this.adds({ ti: this.qlist, daan: this.q, total: this.score });
+    },
+  },
+};
+</script>
